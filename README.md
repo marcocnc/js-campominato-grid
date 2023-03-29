cbc5e7ad53ba545cc28e44c86ea558f3bbb601df
marcocnc <marcocancelliere56@gmail.com>
2023-03-29 15:42:18 +0200

    end

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro e
 1. Fare un markup provvisorio della griglia coi quadrati che che avrà una classe con display: none con in particolare il bottone al centro;
 2. Per rendere possibile ciò si andrà a richiamare il bottone con l'addEventListener, al cui click comparirà la griglia (togliendo la classe col display: none) e scomparirà il bottone;
 3. Sempre nell'addEventListener del bottone si creerà un ciclo for che genera i quadrati;
+4. A questo punto nel file CSS si crea una classe clicked che avrà uno sfondo azzurro che verrà aggiunta e tolta quando l'utente clicca quel quadrato (toggle);
+5. Ora al click del quadrato dovrà comparire in console il numero della cella cliccata, quindi il numero non sarà visibile all'utente.
---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Variabili
+const btnStart  = document.querySelector('.btn-start');
+const container = document.querySelector('.container');
+
+
+// Richiamo bottone
+btnStart.addEventListener('click', function(){
+
+    // metto la classe hide al bottone per nasconderlo
+    btnStart.classList.add('hide');
+
+    // tolgo la classe hide al container per visualizzarlo
+    container.classList.remove('hide');
+
+    for (let i = 1; i <= 100; i++) {
+
+        // creo i div con classe square per aggiungere i quadrati al container
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+        
+
+        // richiamo i quadrati dicendo che al click dell'utente aggiungeranno o toglieranno la classe clicked
+        square.addEventListener('click', function(){
+            this.classList.toggle('clicked');
+            square.squareId = i;
+            console.log(square);
+        })
+    }
+
+})
